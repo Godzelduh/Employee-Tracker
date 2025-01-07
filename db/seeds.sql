25075e1aba65c552a9eeca5fd5f504594d6a3e47
Patrick Bowman <patrickdbowman91@gmail.com>
2025-01-06 22:16:17 -0500

    Misc

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO departments (department_name)
+INSERT INTO Department (Department_name)
 VALUES ('Engineering'),
        ('Marketing'),
        ('Sales'),
@@ -6,7 +6,7 @@ VALUES ('Engineering'),
        ('Finance'),
        ('Legal');
 
-INSERT INTO roles (role_name, department_id, salary)
+INSERT INTO Role (Role_name, Department_id, salary)
 VALUES  ('Software Engineer', 1, 100000),
         ('Marketing Manager', 2, 80000),
         ('Sales Manager', 3, 90000),
@@ -14,19 +14,11 @@ VALUES  ('Software Engineer', 1, 100000),
         ('Finance Manager', 5, 90000),
         ('Legal Counsel', 6, 100000);
 
-INSERT INTO employees (employee_name)
+INSERT INTO Employee (Employee_name)
 VALUES ('John Doe'),
        ('Jane Doe'),
        ('Alice Johnson'),
        ('Bob Johnson'),
        ('Charlie Smith'),
        ('David Smith');
-
-INSERT INTO info (employee_id, info)
-VALUES (1, 'John Doe is a software engineer'),
-       (2, 'Jane Doe is a software engineer'),
-       (3, 'Alice Johnson is a software engineer'),
-       (4, 'Bob Johnson is a software engineer'),
-       (5, 'Charlie Smith is a software engineer'),
-       (6, 'David Smith is a software engineer');
        
---
+++ src/server.ts
@@ -0,0 +1,17 @@
+/*
+import express from 'express';
+import { pool, connectToDb } from './connection.js';
+
+await connectToDb();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+async function appLaunch() {{
+
+}}
+*/
